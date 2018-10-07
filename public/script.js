63589db91581cb4a20cbfd376bc49277fc4d801f
Leandro.Carolino <carolino.leandro@gmail.com>
2018-10-07 21:49:40 +0200

    initial commit

---
+++ public/script.js
@@ -0,0 +1,37 @@
+var PRICE = 9.99;
+new Vue({
+   el: '#app' ,
+   data: {
+       total: 0,
+       items: [
+           { id:1, title: 'item 1'},
+           { id:2, title: 'Item 2'},
+           { id:3, title: 'item 3'}
+       ],
+       cart: []
+   },
+   methods: {
+     addItem: function(index) {
+         this.total += 9.99;
+         let item = this.items[index];
+         let found = false;
+         for(let i = 0; i < this.cart.length; i++){
+             if (this.cart[i].id === item.id){
+                 found = true;
+                 this.cart[i].qty++;
+             }
+         }
+         if(!found){
+            this.cart.push({
+                id: item.id,
+                title: item.title,
+                qty: 1,
+                price: PRICE
+             })
+         }
+         //this.cart.push(this.items[index]);
+        
+         
+     }  
+   }
+});
